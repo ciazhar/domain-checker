48566608f1c28220153f20d30bdedf091ab75d8e
ciazhar <ciazhar.id@gmail.com>
2018-02-05 18:14:37 +0700

    implement launcher in main verticle

--- src/main/java/com/ciazhar/domainchecker/MainVerticle.kt
+++ src/main/java/com/ciazhar/domainchecker/MainVerticle.kt
@@ -5,11 +5,8 @@ import io.vertx.core.AbstractVerticle
 class MainVerticle : AbstractVerticle() {
 
     override fun start() {
-        // your code goes here...
-        vertx.createHttpServer().requestHandler { req ->
-            req.response()
-                    .putHeader("content-type", "text/plain")
-                    .end("Hello from Vert.x!")
-        }.listen(8080)
+
+        print(DomainChecker.check("google.com"))
+
     }
 }
